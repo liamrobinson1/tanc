e423c9ecf62e8f4ba3bcff3b6df2d6482d4ec1cc
Liam Robinson <robin502@purdue.edu>
2024-01-26 21:24:43 -0500

    added deps

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ ext_modules = [
         name="tanc",
         sources=["src/python_bindings.cpp"],
         include_dirs=['src', numpy.get_include(), '/opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3'],
+        build_requires=["pybind11>=2.11.0"
+                        "numpy>=1.26.0"
+                        ],
     ),
 ]
 
