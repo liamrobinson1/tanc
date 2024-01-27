44ce7dd57e2ab8deabab8b150dc4c2ea6222e958
Liam Robinson <robin502@purdue.edu>
2024-01-26 23:43:06 -0500

    updated key

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup
 from pybind11.setup_helpers import Pybind11Extension
 import eigency
+import platform
+
+std_arg = '-std=c++17'
+if platform.system() == 'Windows':
+    std_arg = '/std:c++17'
 
 ext_modules = [
     Pybind11Extension(
         name="sidereal",
         sources=["src/python_bindings.cpp"],
         include_dirs=['src', *tuple(eigency.get_includes())],
-        extra_compile_args=['-std=c++11'],
+        extra_compile_args=[std_arg],
     ),
 ]
 
