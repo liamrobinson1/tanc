305341e718ea3859027976057b611cb457952653
Liam Robinson <robin502@purdue.edu>
2024-08-22 17:08:11 -0400

    testing actions

--- new.py
+++ new.py
@@ -3,4 +3,12 @@ import sidereal
 dtime1 = sidereal.DateTime(2018, 1, 1)
 dtime2 = sidereal.DateTime(2018, 1, 2)
 
+dtimes = sidereal.linspace(dtime1, dtime2, int(1e6))
+
 print(sidereal.seconds(10))
+
+print(dtime2 - dtime1)
+
+print(dir(dtime1))
+
+gast = dtimes.gast()
